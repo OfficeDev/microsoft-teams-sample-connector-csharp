1530f41b0ce62c2d501c8cc12090858f4298ae4b
Wajeed Shaikh <v-washai@microsoft.com>
2018-01-17 15:32:43 +0530

    Created new MVC web application to create and view tasks. Added Connector configuration and Registration code.

---
+++ TeamsToDoAppConnector/Models/Subscription.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamsToDoAppConnector.Models
+{
+    /// <summary>
+    /// Represents the model to store channel subscriptions.
+    /// </summary>
+    public class Subscription
+    {
+        public string GroupName { get; set; }
+        public string WebHookUri { get; set; }
+    }
+}
